b53dd4da1596ce0a2565ed533b8bec211ae2fc6e
sakacond <sakacond@fel.cvut.cz>
2022-10-24 21:36:41 +0200

    line draw func (unfinished)

--- sgl/src/sgl_renderer.cpp
+++ sgl/src/sgl_renderer.cpp
@@ -12,12 +12,32 @@ SglRenderer::SglRenderer() :
 
 SglRenderer::~SglRenderer() {}
 
+template<typename Base, typename T>
+inline bool instanceof(const T *ptr) {
+   return dynamic_cast<const Base*>(ptr) != nullptr;
+}
+
+void draw_line(const SglVertex & start_v, const SglVertex & end_v) {
+    float x_s = start_v.vert[0];
+    float y_s = start_v.vert[1];
+    float x_e = end_v.
+}
+
+
 void SglRenderer::push_vertex(const SglVertex & vertex)
 {
+    vertices.push_back(vertex);
+    
+    if ((state.element_type_mode == SGL_LINE) & (vertices.size() == 2)) {
+        draw_line(vertices[0], vertices[1]);
+        vertices.clear();
+    }
+    // else if ((state.element_type_mode ==SGL_LINE_STRIP ))
 }
 
 void SglRenderer::recording_start()
 {
+    // init vertices?
 }
 
 void SglRenderer::recording_end()
